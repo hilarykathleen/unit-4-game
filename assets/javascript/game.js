4e9b8344deeefed2ba3ad3996c2dd7107e59de46
Hilary Lamb <hilaryklamb@gmail.com>
2019-09-03 21:28:53 -0400

    on.click buttons and additional css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,9 @@ var randomNumber = Math.floor(Math.random()*101+19);
 $('#random-number-text').text(randomNumber);
 console.log(randomNumber);
 
+
+
+
 //Random numbers for crystals between 1 and 12
 var crystal1 = Math.floor(Math.random()*11+1);
 console.log(crystal1);
@@ -21,46 +24,59 @@ var userScore = 0;
 var winCounter = 0;
 var lossCounter = 0;
 
-var userScore = document.getElementById('players-score-text');
-var randomNumberText = document.getElementById('random-number-text');
-var winsText = document.getElementById('wins-text');
-var lossesText = document.getElementById('losses-text');
+// var randomNumberText = document.getElementById('random-number-text');
+// var winsText = document.getElementById('wins-text');
+// var lossesText = document.getElementById('losses-text');
 
 // FUNCTIONS
 
 $(document).ready(function(){
+    $('#wins-text').text(winCounter);
+    console.log("you win");
+    $('#losses-text').text(lossCounter);
+    console.log("you lose");
 
 // Crystal clicks 
-$( "#green" ).click(function() {
-    crystal1 + crystal1 === userScore;
+$("#green").on("click", function() {
+    userScore += crystal1;
     $('#players-score-text').text(userScore);
     console.log(userScore);
 })
 
 $( "#blue" ).click(function() {
-    crystal2 + userScore === userScore;
+    userScore += crystal2;
     $('#players-score-text').text(userScore);
     console.log(userScore);
 })
 
 $( "#yellow" ).click(function() {
-    crystal3 + userScore === userScore;
+    userScore += crystal3;
     $('#players-score-text').text(userScore);
     console.log(userScore);
 })
 
 $( "#red" ).click(function() {
-    crystal4 + userScore === userScore;
+    userScore += crystal4;
     $('#players-score-text').text(userScore);
     console.log(userScore);
 })
 
     // Reset
     
-    // Testing / Debugging
+    
 
 
 // MAIN PROCESS
+
+if (userScore === randomNumber) {
+    winCounter++;
+    $('#wins-text').text(winCounter);
+    console.log("winCounter");
+
+} else { 
+
+}
+   
 
 });
 
